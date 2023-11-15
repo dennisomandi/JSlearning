72d04d9988b90218add72db1f78c2a579d1a4a5a
dennisomandi <ormadydennis@gmail.com>
2023-11-15 22:02:09 +0300

    use tenary operator

--- bingo.js
+++ bingo.js
@@ -1,6 +1,20 @@
+/* write a simple code that prompts the user
+to enter a number and if the number is greater than 90 
+or less that 100. alert the user 
+with a Bingo message. Else alert miss.
+*/
+
 let number = prompt("Enter a number:", 0);
 if (number > 90 && number < 100){
 	alert("BIngo");
 } else {
 	alert("Miss");
 }
+
+/*  
+Now lets write the same code but remmove if and use the tenary operator
+*/
+
+let number = prompt("Enter a number:", 0);
+let message = number > 90 && number < 100 ? "Bingo" : "Miss";
+alert(message);
