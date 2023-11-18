23c56ddb93e7300e595d7325e162ea5d75d3ca7a
dennisomandi <ormandydennis@gmail.com>
2023-11-18 18:17:51 +0300

    loops

---
+++ loops.js
@@ -0,0 +1,8 @@
+// here I am doing some loops exercise. //
+
+//TASK 1 //
+// Write a piece of code that will write numbers from 100 to 0 to the console, but in steps of 10; so 100, 90, 80... etc.//
+
+for (i = 100; i >=0; i -= 10){
+    console.log(i);
+}
