d8d8d13eb783c1534e9a3114e5ba6d6eb4a24eaf
dennisomandi <ormandydennis@gmail.com>
2023-11-18 18:30:18 +0300

    Prompt the user

--- loops.js
+++ loops.js
@@ -6,3 +6,14 @@
 for (i = 100; i >=0; i -= 10){
     console.log(i);
 }
+
+/* Modify the previous program so that it asks the user for the first and last number it uses instead of 100 and 0 (hint: use the prompt dialog).
+Check if the entered values are correct (that the initial value is greater than the final value). */
+
+let UpperLimit = Number(prompt("Enter a number", 0));
+let LowerLimit = Number(prompt("Enter a number", 0));
+
+if (!Number.isNaN(UpperLimit) && !Number.isNaN(LowerLimit) && UpperLimit > LowerLimit);
+for (i = UpperLimit; i >= LowerLimit; i -= 10){
+    console.log(i);
+}
