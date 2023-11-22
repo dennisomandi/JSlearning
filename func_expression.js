c90ae6ebd8aed7790539db854ae9fb6d7b991e32
dennisomandi <ormandydennis@gmail.com>
2023-11-22 20:43:58 +0300

    shorten

--- func_expression.js
+++ func_expression.js
@@ -8,3 +8,13 @@ function add(a, b) {
 let myAdd = add;
 console.log(myAdd(10, 20)); 	// -> 30
 console.log(add(10, 20));	// -> 30
+
+// We can shorten this notation and declare the function by storing it in a variable //
+
+let mAdd = function add(a, b) {
+    return a + b;
+}
+
+console.log(mAdd(10, 20)); // -> 30
+console.log(add(10, 20)); // -> 30
+
