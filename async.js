24562ea75dd7b981d21b0ee51447792f94106639
dennisomandi <ormandydennis@gmail.com>
2023-12-08 20:18:39 +0300

    new async

---
+++ async.js
@@ -0,0 +1,16 @@
+/* Asynchronous programming is a technique that enables your program to start a potentially long-running
+ task and still be able to be responsive to other events while that task runs, 
+ rather than having to wait until that task has finished. 
+Once that task has finished, your program is presented with the result.
+*/
+
+function myDisplayer(something) {
+    document.getElementById("demo").innerHTML = something;
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 5, myDisplayer);
